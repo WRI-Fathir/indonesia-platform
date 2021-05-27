3ca3263629941590bfb1541ae1a7fc667f0ca294
Anggia Riksa Ramadhan <anggia.ramadhan@gmail.com>
2021-05-28 02:27:31 +0700

    add redis to docker compose

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 2020_09_17_045456) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "role", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
   create_table "commitment_timeline_entries", force: :cascade do |t|
     t.text "text"
     t.text "note"
@@ -211,7 +224,8 @@ ActiveRecord::Schema.define(version: 2020_09_17_045456) do
 
   create_table "historical_emissions_metrics", force: :cascade do |t|
     t.string "name", null: false
-    t.text "unit"
+    t.string "unit", null: false
+    t.index ["name", "unit"], name: "index_historical_emissions_metrics_on_name_and_unit", unique: true
   end
 
   create_table "historical_emissions_records", force: :cascade do |t|
